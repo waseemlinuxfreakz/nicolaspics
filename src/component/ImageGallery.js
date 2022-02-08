42a64f9f39fa8ba597119518cd8fbbf18a30c86d
waseemlinuxfreakz <waseem.linuxfreakz@gmail.com>
2022-02-08 20:34:18 +0500

    changes latest

--- src/component/ImageGallery.js
+++ src/component/ImageGallery.js
@@ -55,7 +55,7 @@ function ImageGallery({ dt, handleload }){
 const downloadedImg = new Image();
 downloadedImg.crossOrigin = "Anonymous";
 downloadedImg.addEventListener("load", imageReceived, false);
-downloadedImg.src = "https://static.remove.bg/remove-bg-web/1edb6b547ebc0098fab06852edf91aa18cfebce0/assets/start_remove-c851bdf8d3127a24e2d137a55b1b427378cd17385b01aec6e59d5d4b5f39d2ec.png";
+downloadedImg.src = dt.img_url;
 
 function imageReceived() {
 let canvas = document.createElement("canvas");
